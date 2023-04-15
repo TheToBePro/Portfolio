5d0b06ce151e92cecf1d805500ceac0fc42b6c5d
TheToBePro <zoolcool839@gmail.com>
2023-04-15 11:15:05 +0200

    another update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ const icon = document.querySelector(".menuIcon");
 icon.addEventListener("click", () => {
   menu.classList.toggle("show");
 });
+document.addEventListener("click", (e) => {
+  const isClickInsideMenu = menu.contains(e.target);
+  const isClickicon = icon.contains(e.target);
+  if (!isClickicon && !isClickInsideMenu && menu.classList.contains("show")) {
+    menu.classList.remove("show");
+  }
+});
