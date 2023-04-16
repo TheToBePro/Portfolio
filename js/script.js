308d44a2c86193338b33b388f74f1b579344ae40
TheToBePro <zoolcool839@gmail.com>
2023-04-16 14:34:05 +0200

    update for today

--- js/script.js
+++ js/script.js
@@ -1,13 +1,28 @@
 const menu = document.querySelector(".menu");
 const icon = document.querySelector(".menuIcon");
+const iconx = document.querySelector(".iconx");
 
 icon.addEventListener("click", () => {
-  menu.classList.toggle("show");
+  menu.classList.add("show");
+  icon.classList.add("disppear");
+  iconx.classList.add("appear");
+});
+iconx.addEventListener("click", () => {
+  menu.classList.remove("show");
+  icon.classList.remove("disppear");
+  iconx.classList.remove("appear");
 });
 document.addEventListener("click", (e) => {
   const isClickInsideMenu = menu.contains(e.target);
   const isClickicon = icon.contains(e.target);
   if (!isClickicon && !isClickInsideMenu && menu.classList.contains("show")) {
     menu.classList.remove("show");
+    iconx.classList.remove("appear");
+    icon.classList.remove("disppear");
   }
+});
+
+window.addEventListener("scroll", () => {
+  let nav = document.querySelector("nav");
+  nav.classList.toggle("sticky", window.scrollY > 0);
 });
